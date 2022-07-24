a686790bf0bfbfe553d4cf62bbf9ddb49601fbe0
Swati <swatireddy0229@gmail.com>
2022-07-24 22:29:21 +0530

    SCM-MVC Proj
    SCM-MVC Proj

---
+++ MyFirstMvc/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyFirstMvc.Models;
+
+namespace MyFirstMvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult EmployeeInfo()
+        { 
+            Employee employee= new Employee()
+        {
+            EmployeeId = 216547,
+            EmployeeName = "Swathi",
+            EmployeeLocation = "Bangalore"
+        };
+                return View(employee);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# SCM-MVC proj
+ Handson
